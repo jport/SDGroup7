c8b4214ceba1265b4818caa19646cd295ac269ab
jport <jasonlportillo@gmail.com>
2019-01-20 15:16:51 -0500

    Created the base database models

--- RecipeTop/main/models.py
+++ RecipeTop/main/models.py
@@ -1,3 +1,75 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=500)
+    estDuration = models.IntegerField(default=0)
+    avgDuration = models.IntegerField(default=0)
+    difficulty = models.IntegerField(default=0)
+    rating = models.IntegerField(default=0)
+
+    ingredients = models.ManyToManyField('Ingredient', through='RecipeToIngredient', through_fields=('recipe', 'ingredient'))
+    utensils = models.ManyToManyField('Utensil')
+    
+    def __str__(self):
+        return self.title + " " + str(self.ingredients)
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class RecipeToIngredient(models.Model):
+    recipe = models.ForeignKey('Recipe', on_delete=models.CASCADE)
+    ingredient = models.ForeignKey('Ingredient', on_delete=models.CASCADE)
+    quantity = models.FloatField()
+    unit = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.recipe) + " " + str(self.ingredient) + " " + str(self.quantity) + " " + str(self.unit)
+
+class Utensil(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class RecipeStep(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='steps')
+    stepNumber = models.IntegerField(default=0)
+    text = models.CharField(max_length=2000)
+    weighAction = models.BooleanField(default=False)
+    weighValue = models.IntegerField(default=0)
+    timeAction = models.BooleanField(default=False)
+    timeValue = models.IntegerField(default=0)
+    preheatAction = models.BooleanField(default=False)
+    preheatValue = models.IntegerField(default=0)
+
+    def __str__(self):
+        return str(self.stepNumber) + " " + str(self.recipe)
+
+class User(models.Model):
+    userName = models.CharField(max_length=50)
+    age = models.IntegerField(default=0)
+    likedRecipes = models.ManyToManyField(Recipe)
+
+    def __str__(self):
+        return self.userName + " " + str(self.age) + " " + str(self.likedRecipes)
+
+class History(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name='recipeHistory')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='userHistory')
+    timeOfStart = models.DateTimeField()
+    timeOfEnd = models.DateTimeField()
+
+
+
+# examples:
+# get all recipes Recipe.objects.all()
+# get top 10 recipes Recipe.objects.all()[:10]
+# get steps related to a recipe
+# r = Recipe.objects.all()[0]
+# r.steps.all()
