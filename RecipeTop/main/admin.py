341a37d94cee3eaae7d05486d252bf392cdc1bbb
jport <jasonlportillo@gmail.com>
2019-01-20 15:23:21 -0500

    Registered the models on the admin page

--- RecipeTop/main/admin.py
+++ RecipeTop/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, RecipeStep, RecipeToIngredient, User, Utensil, History
 
 # Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeStep)
+admin.site.register(RecipeToIngredient)
+admin.site.register(User)
+admin.site.register(Utensil)
+admin.site.register(History)
