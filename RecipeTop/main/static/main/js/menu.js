71131fce2bf62ec782538becb702cbf4d30b3bcf
MiguelARam27 <garandster27@live.com>
2019-02-11 10:46:53 -0500

    layout change
    -Made users it's own page
    -added menu.js

---
+++ RecipeTop/main/static/main/js/menu.js
@@ -0,0 +1,42 @@
+let menu_icon =document.getElementById('MenuIcon');
+let menu = document.getElementById('MainMenu');
+let close_button= document.getElementById('close');
+
+function showmenu(){
+    menu_icon.style.display="none";
+    menu.style.left='0px';
+    menu.style.clipPath='polygon(0 0,100% 0,100% 100%,0% 100%)';
+}
+
+function hidemenu(){
+    menu_icon.style.display="block";
+    menu.style.left="-300px";
+    menu.style.clipPath="polygon(0 0,0% 0,100% 100%,0% 100%)";
+}
+
+//menu icon clicked to bring up menu
+menu_icon.addEventListener('click',function(){ 
+    setTimeout(showmenu,100);
+});
+
+//"close" img clicked to close menu
+close_button.addEventListener('click',function(){
+    setTimeout(hidemenu,100);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    //carousel initializer
+    let elems = document.querySelectorAll('.carousel');
+    let instances = M.Carousel.init(elems);
+
+    //collapsible initializer
+    let coll = document.querySelectorAll('.collapsible');
+    let instanc = M.Collapsible.init(coll);
+    
+
+  });
+
+
+
+
