943c7208b894cd2ffcd22f5f48b3233134d9ddd9
MiguelARam27 <garandster27@live.com>
2019-02-11 21:36:40 -0500

    Added menu
    -added Menu to layout
    -fixed some bugs with menu

--- RecipeTop/main/static/main/js/menu.js
+++ RecipeTop/main/static/main/js/menu.js
@@ -24,7 +24,25 @@ close_button.addEventListener('click',function(){
     setTimeout(hidemenu,100);
 });
 
+function set_time(){
+    let date = new Date();
+    let hour = date.getHours();
+    let minutes =date.getMinutes();
+    if(hour>12){
+        hour-=12;
+    }
+    
 
+    document.getElementById("time").innerHTML=hour+":"+minutes;
+}
+
+//call time function every 1 second
+setInterval(() => {set_time();}, 1000);
+
+
+
+
+//materialize stuff
 document.addEventListener('DOMContentLoaded', function() {
     //carousel initializer
     let elems = document.querySelectorAll('.carousel');
@@ -33,9 +51,7 @@ document.addEventListener('DOMContentLoaded', function() {
     //collapsible initializer
     let coll = document.querySelectorAll('.collapsible');
     let instanc = M.Collapsible.init(coll);
-    
-
-  });
+});
 
 
 
