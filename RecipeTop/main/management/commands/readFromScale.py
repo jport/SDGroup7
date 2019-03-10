3e4489e3e70f698996f0c3e98b9b98c93f558f68
jport <jasonlportillo@gmail.com>
2019-03-09 20:09:05 -0500

    Added state to readFromScale

--- RecipeTop/main/management/commands/readFromScale.py
+++ RecipeTop/main/management/commands/readFromScale.py
@@ -8,16 +8,19 @@ TCP_IP = '192.168.43.248'
 TCP_PORT = 2050
 BUFFER_SIZE = 1024
 
+# State information
+IDLE = 0
+READING = 1
+
 class Command(BaseCommand):
     help = "Reads scale over wifi and sends on Django Channel"
 
     def handle(self, *args, **options):
 
-        value = 0;
-        channel_layer = channels.layers.get_channel_layer()
-        group_name = 'scale_coms'
-        async_to_sync(channel_layer.group_add)(
-            group_name,
+        self.channel_layer = channels.layers.get_channel_layer()
+        self.group_name = 'scale_coms'
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name,
             'worker_channel'
         )
 
@@ -31,17 +34,45 @@ class Command(BaseCommand):
         con, addr = s.accept()
         print ('Connection address:', addr)
 
-        while True:
-            #print ("before")
-            data = con.recv(BUFFER_SIZE)
-            #print("hi")
-            if not data: break
+        state = IDLE
 
-            self.stdout.write("Value: " + data.decode('utf-8'))
-            async_to_sync(channel_layer.group_send)(
-                group_name,
-                {
-                    'type': 'scale_message',
-                    'message': data.decode('utf-8')
-                }
-            )
+        while True:
+
+            if state == IDLE:
+                # Check for start
+                if self.checkToggle():
+                    # TODO: Send start to scale
+                    state = READING
+            else:
+                # Check for end
+                if self.checkToggle():
+                    # TODO: Send stop to scale
+                    state = IDLE
+                    continue
+
+                data = con.recv(BUFFER_SIZE)
+                if not data:
+                    continue
+
+                self.stdout.write("Value: " + data.decode('utf-8'))
+                async_to_sync(self.channel_layer.group_send)(
+                    self.group_name,
+                    {
+                        'type': 'scale_message',
+                        'message': data.decode('utf-8')
+                    }
+                )
+
+    def checkToggle(self):
+        try:
+            # Keep reading while message queue not empty
+            while(True):
+                msg = async_to_sync(self.channel_layer.recieve)(
+                    self.group_name
+                )
+
+                if msg['type'] == "toggle":
+                    return True
+        except:
+            # Message queue was empty
+            return False
