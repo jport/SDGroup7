006d4227da049bb279c973bb8b334583be2ca4a9
gcversfeld <gcversfeld@gmail.com>
2019-03-13 13:59:55 -0400

    Make Follow and follow steps

--- RecipeTop/main/views.py
+++ RecipeTop/main/views.py
@@ -89,5 +89,15 @@ def follow(request, recipe_id=0):
 
     context = {
         'recipe':recipe
+
     }
     return render(request, 'main/follow.html',context)
+
+def follow_steps(request, recipe_id=0):
+    recipe=get_object_or_404(Recipe,pk=recipe_id)
+
+    context = {
+        'recipe':recipe
+        
+    }
+    return render(request, 'main/follow_steps.html',context)
