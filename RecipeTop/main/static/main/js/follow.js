ffe043b77a7a01562bae4592e19171dafd6b7b39
gcversfeld <gcversfeld@gmail.com>
2019-03-14 18:03:56 -0400

    color changes and action button

--- RecipeTop/main/static/main/js/follow.js
+++ RecipeTop/main/static/main/js/follow.js
@@ -6,3 +6,12 @@ let start_buttons = document.getElementById("start_steps");
 start_buttons.addEventListener("click", function() {
       window.location.href="/follow_steps/" + recipe_id;
     });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left',
+      hoverEnabled: false
+    });
+  });
