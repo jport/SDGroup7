fd0892b381230939c99035d591a6e711853d4408
jport <jasonlportillo@gmail.com>
2019-03-13 22:32:56 -0400

    Created new API app. Added titles to pages

--- RecipeTop/main/static/main/js/hearted.js
+++ RecipeTop/main/static/main/js/hearted.js
@@ -5,8 +5,17 @@ let hearted = document.getElementsByClassName('hearted');
 for(let i=0;i<hearted.length;i++){
     hearted[i].addEventListener("click",function(){
         if(hearted[i].innerHTML==='favorite_border')
+        {
+            //favorite
+            let curId = hearted[i].parentElement.parentElement.parentElement.id;
             hearted[i].innerHTML='favorite';
+
+            
+        }
         else
+        {
+            //unfavorite
             hearted[i].innerHTML='favorite_border';
+        }
     });
 }
