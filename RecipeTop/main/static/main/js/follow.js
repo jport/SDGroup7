b786ceaf0da2c76e03e0344c581c596b8a45fd93
gcversfeld <gcversfeld@gmail.com>
2019-03-14 20:59:27 -0400

    make a functional button in my popup window!!!

--- RecipeTop/main/static/main/js/follow.js
+++ RecipeTop/main/static/main/js/follow.js
@@ -1,3 +1,4 @@
+//var { Timer } = require('/lib/easytimer/dist/easytimer.min.js');
 
 let start_buttons = document.getElementById("start_steps");
 //let recipe_id = 
@@ -17,20 +18,58 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
+
+
   // Drag anywhere
 
-  $(document).ready(function() {
+$(document).ready(function() {
+
   
-  new jBox('Modal', {
-    attach: $('#modal-drag-anywhere'),
-    width: 220,
-    title: 'jBox',
-    overlay: false,
-    createOnInit: true,
-    content: 'Drag me around by clicking anywhere',
-    draggable: true,
-    repositionOnOpen: false,
-    repositionOnContent: false
-  });
+  	new jBox('Modal', {
+	    attach: $('#timer-drag-anywhere'),
+	    width: 220,
+	    title: 'Timer',
+	    overlay: false,
+	    createOnInit: true,
+	    content:'\
+	    <div> \
+	    	<p>Drag me around by clicking anywhere</p>\
+	    	<br>\
+	    		<button class="btn waves-effect waves-light" type="button" name="action" id="start_timer"> Start Timer </button>\
+	    	</div>\
+	    	<script>var timer_button = document.getElementById("start_timer");</script>\
+	    	<script>timer_button.addEventListener("click", function() {window.location.href="/follow_steps/" + recipe_id;});</script>\
+	    	',
+	    draggable: true,
+	    repositionOnOpen: false,
+	    repositionOnContent: false
+	});
+
+	new jBox('Modal', {
+	    attach: $('#preheat-drag-anywhere'),
+	    width: 220,
+	    title: 'jBox',
+	    overlay: false,
+	    createOnInit: true,
+	    content: 'Drag me around by clicking anywhere',
+	    draggable: true,
+	    repositionOnOpen: false,
+	    repositionOnContent: false
+	});
+	new jBox('Modal', {
+	    attach: $('#scale-drag-anywhere'),
+	    width: 220,
+	    title: 'jBox',
+	    overlay: false,
+	    createOnInit: true,
+	    content: 'Drag me around by clicking anywhere',
+	    draggable: true,
+	    repositionOnOpen: false,
+	    repositionOnContent: false
+  	});
     
 });
+
+
+
+
