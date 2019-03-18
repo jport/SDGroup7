f85bf7493905c73ed94d28b0120275d36324ea17
jport <jasonlportillo@gmail.com>
2019-03-18 13:25:29 -0400

    Moved script outside of string

--- RecipeTop/main/static/main/js/follow.js
+++ RecipeTop/main/static/main/js/follow.js
@@ -40,32 +40,7 @@ $(document).ready(function() {
   			</div>\
 	    	<br>\
 	    	<button class="btn waves-effect waves-light" type="button" name="action" id="start_timer"> Start Timer </button>\
-	    </div>' 
-	    +'<script>\
-	    	var timer_button = document.getElementById("start_timer");\
-	    	var timer_flag = false;\
-			timer_button.addEventListener("click", function() {\
-				document.getElementById("start_timer").classList.add("hide");\
-				var range_val = document.getElementById("test5").value;\
-				var day = Date.now();\
-				var countDownDate = day + range_val*1000*60;\
-				var x = setInterval(function() {\
-	   			var now = new Date().getTime();\
-				var distance = countDownDate - now;\
-				var days = Math.floor(distance / (1000 * 60 * 60 * 24));\
-				var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));\
-				var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));\
-				var seconds = Math.floor((distance % (1000 * 60)) / 1000);\
-				document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";\
-				if (distance < 0) {\
-				    clearInterval(x);\
-				    document.getElementById("demo").innerHTML = "EXPIRED";\
-				} \
-				}, 1000);\
-			});\
-		</script>\
-		<script src={% static "main/js/materialize.min.js" %}></script>\
-	    	',
+	    </div>',
 	    draggable: 'title',
 	    repositionOnOpen: false,
 	    repositionOnContent: false
@@ -93,9 +68,35 @@ $(document).ready(function() {
 	    repositionOnOpen: false,
 	    repositionOnContent: false
   	});
-    
+	
+	var timer_button = document.getElementById("start_timer");
+	timer_button.addEventListener("click", event_handler, 1000);
 });
 
+function foo()
+{
+	alert("hello");
+}
 
 
-
+function event_handler() {
+	alert("event_handler()");
+	document.getElementById("start_timer").classList.add("hide");
+	var range_val = document.getElementById("test5").value;
+	var day = Date.now();
+	var countDownDate = day + range_val*1000*60;
+	var x = setInterval(function() {
+   			var now = new Date().getTime();
+			var distance = countDownDate - now;
+			var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+			var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+			var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+			var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+			document.getElementById("demo").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+			if (distance < 0) {
+				    clearInterval(x);
+				    document.getElementById("demo").innerHTML = "EXPIRED";
+			}
+		}
+	);
+}
