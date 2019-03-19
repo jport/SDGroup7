5800d42eeb941feff3b4edc485c799eab46d8b57
jport <jasonlportillo@gmail.com>
2019-03-18 21:49:03 -0400

    Added hearted AJAX calls

--- RecipeTop/API/views.py
+++ RecipeTop/API/views.py
@@ -1,7 +1,31 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from main.models import *
+import json
 
 # Create your views here.
 def changeHeartStatus(request):
-    print("change")
-    return JsonResponse({'error': ''})
+    error = ""
+    data = json.loads(request.body)
+
+    if not "RecipeID" in data:
+        error = "Missing RecipeID;"
+    if not "Status" in data:
+        error += "Missing Status;"
+
+    if error:
+        return JsonResponse({'error': error})
+
+    recipeId = data["RecipeID"]
+    userId = request.session["userId"]
+    newStatus = data["Status"]
+    
+    recipe = Recipe.objects.filter(id=recipeId)[0]
+    user = User.objects.filter(id=userId)[0]
+
+    if newStatus == 0: # Delete favorite
+        recipe.user_set.remove(user)
+    else: # Add favorite
+        recipe.user_set.add(user)
+    
+    return JsonResponse({'error': error})
--- RecipeTop/main/static/main/js/hearted.js
+++ RecipeTop/main/static/main/js/hearted.js
@@ -4,18 +4,37 @@ let hearted = document.getElementsByClassName('hearted');
 
 for(let i=0;i<hearted.length;i++){
     hearted[i].addEventListener("click",function(){
+        let curRecipeId = hearted[i].parentElement.parentElement.parentElement.id;
+        let jsonPayload = {
+            RecipeID: curRecipeId,
+            Status: 0
+        };
+
         if(hearted[i].innerHTML==='favorite_border')
         {
             //favorite
-            let curId = hearted[i].parentElement.parentElement.parentElement.id;
             hearted[i].innerHTML='favorite';
-
-            
+            jsonPayload.Status = 1;
         }
         else
         {
             //unfavorite
             hearted[i].innerHTML='favorite_border';
         }
+
+        payloadString = JSON.stringify(jsonPayload);
+        console.log(payloadString);
+        $.ajax({
+            type: 'POST',
+            url: '/API/changeHeartStatus',
+            data: payloadString,
+            success: function(data){
+                if(data.error)
+                    alert(error);
+            },
+            error: function(error){
+                alert("Error: " + error)
+            }
+        });
     });
 }
