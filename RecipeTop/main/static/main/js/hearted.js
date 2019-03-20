fb0fbe0c755338bc1a6286258ed3a0c02e69d4ad
jport <jasonlportillo@gmail.com>
2019-03-20 12:05:42 -0400

    Fixed hearts, and added links to execute

--- RecipeTop/main/static/main/js/hearted.js
+++ RecipeTop/main/static/main/js/hearted.js
@@ -4,22 +4,34 @@ let hearted = document.getElementsByClassName('hearted');
 
 for(let i=0;i<hearted.length;i++){
     hearted[i].addEventListener("click",function(){
-        let curRecipeId = hearted[i].parentElement.parentElement.parentElement.id;
+        let curId = hearted[i].id;
+        let curRecipeId = curId.replace("hearted_", "").replace("_prop", "");//hearted[i].parentElement.parentElement.parentElement.id;
         let jsonPayload = {
             RecipeID: curRecipeId,
             Status: 0
         };
 
+        let otherId = curId + "_prop";
+        if(curId.includes("_prop"))
+            otherId = curId.replace("_prop", "");
+
+        let otherHeart = $("#" + otherId)[0];
+        console.log(otherId);
+        console.log(otherHeart);
+        console.log(curRecipeId);
+
         if(hearted[i].innerHTML==='favorite_border')
         {
             //favorite
-            hearted[i].innerHTML='favorite';
+            hearted[i].innerHTML = 'favorite';
+            otherHeart.innerHTML = 'favorite';
             jsonPayload.Status = 1;
         }
         else
         {
             //unfavorite
-            hearted[i].innerHTML='favorite_border';
+            hearted[i].innerHTML = 'favorite_border';
+            otherHeart.innerHTML = 'favorite_border';
         }
 
         payloadString = JSON.stringify(jsonPayload);
