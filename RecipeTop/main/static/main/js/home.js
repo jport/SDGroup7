8ad6d9a23a8f5089ed72de62d33843166d8a7104
MiguelARam27 <garandster27@live.com>
2019-03-26 09:19:44 -0400

    made base location of card
    base-location of home card

---
+++ RecipeTop/main/static/main/js/home.js
@@ -0,0 +1,17 @@
+let cards = document.getElementsByClassName('cards');
+let side_container =document.getElementsByClassName('side_container');
+for(let i=0;i<cards.length;i++){
+    cards[i].addEventListener('click',function(){
+        hide();
+    });
+}
+
+// const hide=()=>{
+//     if(side_container.overflow==='scroll'){
+//         side_container.overflow='visible';
+//         alert('y');
+//     }
+//     else{
+//         side_container.overflow="scroll";
+//     }
+// }
