fac59cf61fcdcf894743b223a3ee318c109732a4
MiguelARam27 <garandster27@live.com>
2019-03-31 20:50:46 -0400

    js for finished page
    added rating fix

--- RecipeTop/main/static/main/js/finish_recipe.js
+++ RecipeTop/main/static/main/js/finish_recipe.js
@@ -1,18 +1,30 @@
 let stars = document.getElementsByClassName('stars');
+let cakes = document.getElementsByClassName('cakes');
 
 for(let i=0;i<stars.length;i++){
     stars[i].addEventListener('click',function(){
-        icon_pic(i);
+        icon_pic(i,stars);
+    });
+}
+for(let i=0;i<cakes.length;i++){
+    cakes[i].addEventListener('click',function(){
+        icon_pic(i,cakes);
     });
 }
 
-const icon_pic=(index)=>{
-    let img_src =stars[index].src;
-    if(img_src.indexOf('2')!=-1){
-      img_src=img_src.replace('2','1');
+const icon_pic=(index,element)=>{
+    // let img_src =stars[index].src;
+    let index_src=element[index].src;
+    if(index_src.indexOf('1')!=-1){
+        let img_src=index_src.replace('1','2');
+        for(let i=index;i<element.length;i++)
+        element[i].src= img_src;
     }
     else{
-      img_src=img_src.replace('1','2');
+        let img_src =index_src.replace('2','1');
+        for(let i=0;i<=index;i++){
+                element[i].src=img_src;
+        }
     }
-    stars[index].src=img_src;
-  };
+};
+
